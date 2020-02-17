b028a15c14214c2a1327f665ffe3527f62f11285
burib <burib@users.noreply.github.com>
2020-02-17 16:11:01 +0100

    update public_api

--- projects/ng-kloten/src/public_api.ts
+++ projects/ng-kloten/src/public_api.ts
@@ -3,13 +3,23 @@
  */
 
 export * from './lib/footer/footer.module';
+export * from './lib/footer/footer.component';
+
 export * from './lib/header/header.module';
+export * from './lib/header/header.component';
+
 export * from './lib/breadcrumbs/breadcrumbs.module';
+export * from './lib/breadcrumbs/breadcrumbs.component';
+
 export * from './lib/sidenav/sidenav.module';
+export * from './lib/sidenav/sidenav.component';
+
 export * from './lib/table/table.module';
+export * from './lib/table/table.component';
 export * from './lib/table/table.header.interface';
 
 export * from './lib/auth/cookie.service';
+export * from './lib/auth/sso-login/sso-login.component';
 export * from './lib/auth/auth.service';
 export * from './lib/auth/auth.guard';
 export * from './lib/auth/auth.module';
